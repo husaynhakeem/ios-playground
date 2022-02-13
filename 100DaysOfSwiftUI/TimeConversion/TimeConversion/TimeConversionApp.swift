42b142bc3cf6e434868f313e6c8f83b966ed5ae5
Husayn Hakeem <husaynhakeem@gmail.com>
2022-02-12 18:06:34 -0800

    Complete challenge 1 (Day 19)

---
+++ 100DaysOfSwiftUI/TimeConversion/TimeConversion/TimeConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeConversionApp.swift
+//  TimeConversion
+//
+//  Created by Husayn Hakeem on 2/12/22.
+//
+
+import SwiftUI
+
+@main
+struct TimeConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
